db43e72957f8e176bb6e21fb81cec30ded7e6dc1
Brennon Loveless <bloveless@jahnelgroup.com>
2022-08-16 07:32:52 -0700

    Enable tree sitter indenting

--- private_dot_config/nvim/lua/brennon/tree-sitter.lua
+++ private_dot_config/nvim/lua/brennon/tree-sitter.lua
@@ -11,6 +11,10 @@ require'nvim-treesitter.configs'.setup {
   -- List of parsers to ignore installing (for "all")
   ignore_install = {},
 
+  indent = {
+    enable = true
+  },
+
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
@@ -39,12 +43,12 @@ require'nvim-treesitter.configs'.setup {
   }
 }
 
----WORKAROUND for "No folds found" error
-vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
-  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
-  callback = function()
-    vim.opt.foldmethod     = 'expr'
-    vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
-  end
-})
+---WORKAROUND for "Folds not found" error
+-- vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
+--   group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+--   callback = function()
+--     vim.opt.foldmethod     = 'expr'
+--     vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
+--   end
+-- })
 ---ENDWORKAROUND
