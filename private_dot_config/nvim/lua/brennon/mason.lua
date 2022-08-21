a69e0b3e104eba029c8353d70a75ba059beb0278
Brennon Loveless <brennon.loveless@gmail.com>
2022-08-20 21:22:36 -0700

    Cleaned up lsp files

--- private_dot_config/nvim/lua/brennon/mason.lua
+++
@@ -1,9 +0,0 @@
-require("mason").setup()
-require("mason-lspconfig").setup({
-    ensure_installed = { "sumneko_lua", "gopls", }, automatic_installation = true
-})
-require('mason-tool-installer').setup({
-    ensure_installed = { "gopls", "gofumpt", "lua-language-server", "yaml-language-server", "typescript-language-server" },
-    auto_update = true,
-    run_on_start = true,
-})
