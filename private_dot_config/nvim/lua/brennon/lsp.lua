be7bd140df6249c271b117259fc87643ba6c124f
Brennon Loveless <bloveless@jahnelgroup.com>
2022-08-22 07:39:53 -0700

    Update lua language server to recognize vim global. Clean up cmp when using luasnip

--- private_dot_config/nvim/lua/brennon/lsp.lua
+++ private_dot_config/nvim/lua/brennon/lsp.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua", "tsserver", "gopls", "yamlls" },
+  ensure_installed = { "sumneko_lua", "tsserver", "gopls", "yamlls", "terraformls" },
   automatic_installation = false,
 })
 require("null-ls").setup({
@@ -45,15 +45,37 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<leader>f',function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
-require('lspconfig')['gopls'].setup{
+require'lspconfig'.gopls.setup{
     on_attach = on_attach,
 }
-require('lspconfig')['sumneko_lua'].setup{
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+require'lspconfig'.tsserver.setup{
     on_attach = on_attach,
 }
-require('lspconfig')['tsserver'].setup{
+require'lspconfig'.yamlls.setup{
     on_attach = on_attach,
 }
-require('lspconfig')['yamlls'].setup{
-    on_attach = on_attach,
+require'lspconfig'.terraformls.setup{
+  on_attach = on_attach,
 }
