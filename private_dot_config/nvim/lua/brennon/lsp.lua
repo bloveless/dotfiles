0a543ed9029a2e0e8b7a25e1f45e4a714dd9a5e9
Brennon Loveless <bloveless@jahnelgroup.com>
2022-08-26 07:42:50 -0700

    Added eslint_d and stylua to mason ensure installed

--- private_dot_config/nvim/lua/brennon/lsp.lua
+++ private_dot_config/nvim/lua/brennon/lsp.lua
@@ -1,12 +1,13 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua", "tsserver", "gopls", "yamlls", "terraformls" },
+  ensure_installed = { "sumneko_lua", "tsserver", "gopls", "yamlls", "terraformls", "eslint_d", "stylua" },
   automatic_installation = false,
 })
 require("null-ls").setup({
     sources = {
         require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.diagnostics.eslint,
+        require("null-ls").builtins.diagnostics.eslint_d,
+        require("null-ls").builtins.code_actions.eslint_d,
         require("null-ls").builtins.completion.spell,
     },
 })
