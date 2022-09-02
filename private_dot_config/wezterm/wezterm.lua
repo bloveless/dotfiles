1907cf1fa18b0cc70199a74fd3df090b2ab2031c
Brennon Loveless <bloveless@jahnelgroup.com>
2022-09-02 13:33:59 -0700

    Added ability to move tabs in absolutely and relatively

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,7 +1,24 @@
+local wezterm = require 'wezterm'
+
+local mykeys = {
+  { key = '{', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative(-1) },
+  { key = '}', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative(1) },
+}
+
+for i = 1, 8 do
+  -- CTRL+ALT + number to move to that position
+  table.insert(mykeys, {
+    key = tostring(i),
+    mods = 'CTRL|ALT',
+    action = wezterm.action.MoveTab(i - 1),
+  })
+end
+
 return {
   color_scheme = "Gruvbox dark, medium (base16)",
   font_size = 13,
   initial_cols = 140,
   initial_rows = 40,
   scrollback_lines = 4000,
+  keys = mykeys,
 }
