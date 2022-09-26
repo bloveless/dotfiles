8e1dbfab2ee9847ffc1a81b3463114f050c4dbd7
Brennon Loveless <bloveless@jahnelgroup.com>
2022-09-26 11:32:25 -0700

    Confirm before creating data node

--- private_dot_config/nvim/after/plugin/mind.lua
+++ private_dot_config/nvim/after/plugin/mind.lua
@@ -1,8 +1,27 @@
 require("mind").setup({
   ui = {
-    width = 50
-  }
+    width = 50,
+  },
+  tree = {
+    automatic_data_creation = false,
+  },
+  keymaps = {
+    normal = {
+      ["<cr>"] = function(args)
+        require("mind.ui").with_confirmation("Are you sure you'd like to create a new data node?", function()
+          require("mind.commands").commands.open_data(args)
+        end)
+      end,
+    },
+    selection = {
+      ["<cr>"] = function(args)
+        require("mind.ui").with_confirmation("Are you sure you'd like to create a new data node?", function()
+          require("mind.commands").commands.open_data(args)
+        end)
+      end,
+    },
+  },
 })
 
-vim.keymap.set('n', '<leader>mp', require("mind").open_project)
-vim.keymap.set('n', '<leader>mg', require("mind").open_main)
+vim.keymap.set("n", "<leader>mp", require("mind").open_project)
+vim.keymap.set("n", "<leader>mg", require("mind").open_main)
