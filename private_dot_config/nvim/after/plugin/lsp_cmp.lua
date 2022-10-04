385c8d59ab2b46e9eeaa6a10b4215d4957d7398f
Brennon Loveless <bloveless@jahnelgroup.com>
2022-10-04 13:30:44 -0700

    Back to lsp-zero

---
+++ private_dot_config/nvim/after/plugin/lsp_cmp.lua
@@ -0,0 +1,49 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.set_preferences({
+  set_lsp_keymaps = false
+})
+
+lsp.on_attach(function(client, bufnr)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set("n", "<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "<leader>fo", function()
+    vim.lsp.buf.format({ async = true })
+  end, bufopts)
+end)
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+lsp.setup_nvim_cmp({
+  mapping = lsp.defaults.cmp_mappings({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  })
+})
+
+lsp.nvim_workspace()
+lsp.setup()
+
--- private_dot_config/nvim/lua/brennon/plugins.lua
+++ private_dot_config/nvim/lua/brennon/plugins.lua
@@ -65,24 +65,27 @@ require("packer").startup(function(use)
     },
   })
 
-  -- LSP Support
-  use({ "neovim/nvim-lspconfig" })
-  use({ "williamboman/mason.nvim" })
-  use({ "WhoIsSethDaniel/mason-tool-installer.nvim" })
-  use({ "williamboman/mason-lspconfig.nvim" })
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
 
-  -- Autocompletion
-  use({ "hrsh7th/nvim-cmp" })
-  use({ "hrsh7th/cmp-buffer" })
-  use({ "hrsh7th/cmp-cmdline" })
-  use({ "hrsh7th/cmp-path" })
-  use({ "saadparwaiz1/cmp_luasnip" })
-  use({ "hrsh7th/cmp-nvim-lsp" })
-  use({ "hrsh7th/cmp-nvim-lua" })
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
 
-  -- Snippets
-  use({ "L3MON4D3/LuaSnip" })
-  use({ "rafamadriz/friendly-snippets" })
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
 
   use("tpope/vim-surround")
   use("tpope/vim-obsession")
