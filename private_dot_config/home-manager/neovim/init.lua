43eed8e67a747cd0354d5358051b08377426bac4
Brennon Loveless <brennon.loveless@gmail.com>
2023-06-25 07:22:22 -0700

    chore: more neovim lazy updates

--- private_dot_config/home-manager/neovim/init.lua
+++ private_dot_config/home-manager/neovim/init.lua
@@ -24,7 +24,7 @@ require('lazy').setup({
   -- { "catppuccin/nvim" }
   {
     "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
@@ -40,9 +40,7 @@ require('lazy').setup({
       'neovim/nvim-lspconfig',
       {
         'williamboman/mason.nvim',
-        command = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
+        build = ":MasonUpdate"
       },
       'williamboman/mason-lspconfig.nvim',
       {
@@ -62,9 +60,10 @@ require('lazy').setup({
     },
   },
 
-  { -- Highlight, edit, and navigate code
+  {
+    -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    command = function()
+    build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
   },
@@ -90,7 +89,7 @@ require('lazy').setup({
   'tpope/vim-sleuth',                    -- Detect tabstop and shiftwidth automatically
 
   -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 'nvim-telescope/telescope.nvim',            branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
