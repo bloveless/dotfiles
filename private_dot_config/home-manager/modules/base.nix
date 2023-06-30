a4958692ae4afe07ce008550df1ebf7dba59ca1b
Brennon Loveless <bloveless@jahnelgroup.com>
2023-06-29 17:02:49 -0700

    chore: update home-manager. updated zsh syntax enabled syntax

--- private_dot_config/home-manager/modules/base.nix
+++ private_dot_config/home-manager/modules/base.nix
@@ -57,7 +57,7 @@ in
     enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
     historySubstringSearch.enable = true;
     defaultKeymap = "emacs";
 
