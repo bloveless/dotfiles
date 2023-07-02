95ec4391ea94e058ddd87dd2f79aab7e07b87380
Brennon Loveless <brennon.loveless@gmail.com>
2023-07-02 10:25:27 -0700

    feat: Remove unused packages from home and devbox

--- private_dot_config/home-manager/hm-devbox.nix
+++ private_dot_config/home-manager/hm-devbox.nix
@@ -1,39 +1,23 @@
 { config, fetchFromGitHub, nixpkgs, pkgs, ... }:
 {
   # environment variables
-  home.sessionVariables = {
-    NOMAD_ADDR = "http://192.168.5.20:4646";
-    CONSUL_HTTP_ADDR = "192.168.5.20:8500";
-  };
-
   home.packages = [
     pkgs.ripgrep
     pkgs.git
     pkgs.file
     pkgs.chezmoi
-    pkgs.awscli2
-    pkgs.k9s
-    pkgs.terraform
     pkgs.curl
     pkgs.gnumake
-    pkgs.go
-    pkgs.golangci-lint
     pkgs.talosctl
     pkgs.argocd
     pkgs.jq
-    pkgs.nomad
-    pkgs.consul
     pkgs.ansible
     pkgs.bind # provides nslookup
     pkgs.postgresql_15 # provides psql
     pkgs.kubectl
     pkgs.kubernetes-helm
-    pkgs.tfk8s
-    pkgs.skaffold
     pkgs.kubeseal
     pkgs.cilium-cli
-    pkgs.helix
-    pkgs.rustup
   ];
   
   programs.zsh = {
--- private_dot_config/home-manager/hm-home.nix
+++ private_dot_config/home-manager/hm-home.nix
@@ -3,21 +3,10 @@
   home.packages = [
     pkgs.git
     pkgs.chezmoi
-    pkgs.groff
-    pkgs.awscli2
     pkgs.k9s
-    pkgs.ssm-session-manager-plugin
-    pkgs.terraform
     pkgs.ripgrep
     pkgs.curl
-    pkgs.hugo
-    pkgs.go
-    pkgs.golangci-lint
     pkgs.jq
-    nixpkgs.legacyPackages.x86_64-linux.qmk
-    pkgs.helix
-    pkgs.rustup
-    pkgs.cargo-lambda
   ];
 
   # home.shellAliases = {
