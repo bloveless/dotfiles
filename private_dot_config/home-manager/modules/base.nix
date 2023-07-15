452127c9dbb5bfd6f871e6b5204fa6c3b46b7349
Brennon Loveless <brennon.loveless@gmail.com>
2023-07-15 13:27:09 -0700

    fix: removed unused settings

--- private_dot_config/home-manager/modules/base.nix
+++ private_dot_config/home-manager/modules/base.nix
@@ -140,11 +140,6 @@
       {
         plugin = tmuxPlugins.catppuccin;
         extraConfig = ''
-          set -g @catppuccin_powerline_icons_theme_enabled on
-          set -g @catppuccin_l_left_separator ""
-          set -g @catppuccin_l_right_separator ""
-          set -g @catppuccin_r_left_separator ""
-          set -g @catppuccin_r_right_separator ""
         '';
       }
     ];
