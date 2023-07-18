2d5bd236e1b6e0b6b09228f1d83e4b1f2b21a783
Brennon Loveless <brennon.loveless@gmail.com>
2023-07-18 15:37:07 -0700

    fix: put treesitter and telescope configs in lazy.lua instead of trying to overwrite the config

--- private_dot_config/home-manager/neovim/lua/config/lazy.lua
+++ private_dot_config/home-manager/neovim/lua/config/lazy.lua
@@ -11,15 +11,13 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -50,3 +48,7 @@ require("lazy").setup({
     },
   },
 })
+
+-- For some reason specifying gcc specifically instead of clang (even though they seem to be the same thing) works for compiling terraform and hcl on Apple M1
+require("nvim-treesitter.install").compilers = { "gcc" }
+require("telescope").load_extension("notify")
