f2a270f91fc4ad480a7e31c04a4645692a4ef4e4
Brennon Loveless <bloveless@jahnelgroup.com>
2023-07-18 05:48:34 -0700

    chore: clean up work packages

--- private_dot_config/home-manager/hm-work.nix
+++ private_dot_config/home-manager/hm-work.nix
@@ -7,23 +7,24 @@ in
     pkgs.git
     pkgs.chezmoi
     pkgs.groff
-    pkgs.awscli2
-    pkgs.ssm-session-manager-plugin
+    # pkgs.awscli2
+    # pkgs.ssm-session-manager-plugin
     pkgs.nodejs
-    pkgs.yarn
-    pkgs.argocd
-    pkgs.nodePackages.aws-cdk
-    pkgs.nodePackages.redoc-cli
-    pkgs.terraform
-    pkgs.terraform-ls
+    # pkgs.yarn
+    # pkgs.argocd
+    # pkgs.nodePackages.aws-cdk
+    # pkgs.nodePackages.redoc-cli
+    # pkgs.terraform
+    # pkgs.terraform-ls
     pkgs.ripgrep
     pkgs.curl
-    pkgs.go
-    pkgs.golangci-lint
+    # pkgs.go
+    # pkgs.golangci-lint
     pkgs.jq
-    extraNodePackages.serverless
+    # extraNodePackages.serverless
     pkgs.cachix
     pkgs.fd # for neovim telescope
+    pkgs.lazygit # for neovim git
   ];
 
   # home.shellAliases = {
