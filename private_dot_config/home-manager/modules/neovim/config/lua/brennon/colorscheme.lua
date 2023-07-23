1a32b96c36d2018ebc7105e149655dfc84bf9ba6
Brennon Loveless <brennon.loveless@gmail.com>
2023-07-23 08:23:38 -0700

    feat: colorscheme updates

--- private_dot_config/home-manager/modules/neovim/config/lua/brennon/colorscheme.lua
+++ private_dot_config/home-manager/modules/neovim/config/lua/brennon/colorscheme.lua
@@ -1,34 +1,32 @@
 require("catppuccin").setup({
     integrations = {
-        cmp = true,
-        gitsigns = true,
-        neotree = true,
-        treesitter = true,
-        notify = true,
-        alpha = true,
-        illuminate = true,
-        noice = true,
-        native_lsp = {
-            enabled = true,
-            virtual_text = {
-                errors = { "italic" },
-                hints = { "italic" },
-                warnings = { "italic" },
-                information = { "italic" },
+        integrations = {
+            alpha = true,
+            cmp = true,
+            gitsigns = true,
+            illuminate = true,
+            indent_blankline = { enabled = true },
+            lsp_trouble = true,
+            mason = true,
+            mini = true,
+            native_lsp = {
+                enabled = true,
+                underlines = {
+                    errors = { "undercurl" },
+                    hints = { "undercurl" },
+                    warnings = { "undercurl" },
+                    information = { "undercurl" },
+                },
             },
-            underlines = {
-                errors = { "underline" },
-                hints = { "underline" },
-                warnings = { "underline" },
-                information = { "underline" },
-            },
-            inlay_hints = {
-                background = true,
-            },
-        },
-        navic = {
-            enabled = false,
-            custom_bg = "NONE", -- "lualine" will set background to mantle
+            navic = { enabled = true, custom_bg = "lualine" },
+            neotest = true,
+            noice = true,
+            notify = true,
+            neotree = true,
+            semantic_tokens = true,
+            telescope = true,
+            treesitter = true,
+            which_key = true,
         },
     }
 })
