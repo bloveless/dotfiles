d87dc9be6c3e8a8a9a656545e426c74c26447f69
Brennon Loveless <brennon.loveless@gmail.com>
2023-08-06 22:25:29 -0700

    fix: change trouble open to toggle

--- private_dot_config/home-manager/modules/neovim/config/init.lua
+++ private_dot_config/home-manager/modules/neovim/config/init.lua
@@ -76,7 +76,7 @@ require('lazy').setup({
   'tpope/vim-sleuth',
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',  opts = {} },
+  { 'folke/which-key.nvim', opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -90,8 +90,7 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
-          { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
         vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
         vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
       end,
@@ -201,14 +200,14 @@ require('lazy').setup({
       {
         '<leader>xx',
         function()
-          require('trouble').open()
+          require('trouble').toggle()
         end,
         desc = 'Trouble Toggle',
       },
       {
         '<leader>xw',
         function()
-          require('trouble').open 'workspace_diagnostics'
+          require('trouble').toggle 'workspace_diagnostics'
         end,
         desc = 'Trouble Toggle Workspace',
       },
