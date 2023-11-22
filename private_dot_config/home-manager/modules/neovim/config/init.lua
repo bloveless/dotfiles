169077e47acedf90767994498196eda28f99b42d
Brennon Loveless <brennon.loveless.ext@bayer.com>
2023-11-22 09:21:34 -0800

    feat: change colorscheme to catppuccin-mocha

--- private_dot_config/home-manager/modules/neovim/config/init.lua
+++ private_dot_config/home-manager/modules/neovim/config/init.lua
@@ -114,55 +114,54 @@ require('lazy').setup({
   --   end,
   -- },
 
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'gruvbox'
-    end,
-    opts = {},
-  },
-
-  -- catppuccin
   -- {
-  --   'catppuccin/nvim',
+  --   "ellisonleao/gruvbox.nvim",
   --   priority = 1000,
   --   config = function()
-  --     vim.cmd.colorscheme 'catppuccin-macchiato'
+  --     vim.cmd.colorscheme 'gruvbox'
   --   end,
-  --   name = 'catppuccin',
-  --   opts = {
-  --     integrations = {
-  --       alpha = true,
-  --       cmp = true,
-  --       flash = true,
-  --       gitsigns = true,
-  --       illuminate = true,
-  --       indent_blankline = { enabled = true },
-  --       lsp_trouble = true,
-  --       mason = true,
-  --       mini = true,
-  --       native_lsp = {
-  --         enabled = true,
-  --         underlines = {
-  --           errors = { 'undercurl' },
-  --           hints = { 'undercurl' },
-  --           warnings = { 'undercurl' },
-  --           information = { 'undercurl' },
-  --         },
-  --       },
-  --       navic = { enabled = true, custom_bg = 'lualine' },
-  --       neotest = true,
-  --       noice = true,
-  --       notify = true,
-  --       neotree = true,
-  --       semantic_tokens = true,
-  --       telescope = true,
-  --       treesitter = true,
-  --       which_key = true,
-  --     },
-  --   },
+  --   opts = {},
   -- },
+
+  {
+    'catppuccin/nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-mocha'
+    end,
+    name = 'catppuccin',
+    opts = {
+      integrations = {
+        alpha = true,
+        cmp = true,
+        flash = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { 'undercurl' },
+            hints = { 'undercurl' },
+            warnings = { 'undercurl' },
+            information = { 'undercurl' },
+          },
+        },
+        navic = { enabled = true, custom_bg = 'lualine' },
+        neotest = true,
+        noice = true,
+        notify = true,
+        neotree = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
+    },
+  },
 
   {
     -- Set lualine as statusline
