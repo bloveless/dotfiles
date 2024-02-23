f6b7fd6075a630ee6c92fdf58b03c0a124613bdd
Brennon Loveless <brennon.loveless.ext@bayer.com>
2024-02-23 14:24:42 -0800

    feat: style wezterm tab bar

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -32,4 +32,58 @@ config.scrollback_lines = 50000
 config.keys = mykeys
 config.tab_bar_at_bottom = true
 
+config.window_frame = {
+	-- The font used in the tab bar.
+	-- Roboto Bold is the default; this font is bundled
+	-- with wezterm.
+	-- Whatever font is selected here, it will have the
+	-- main font setting appended to it to pick up any
+	-- fallback fonts you may have used there.
+	font = wezterm.font({ family = "MonaspiceXe Nerd Font", weight = "Bold" }),
+
+	-- The size of the font in the tab bar.
+	-- Default to 10.0 on Windows but 12.0 on other systems
+	font_size = 10.0,
+
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = "#2e3440",
+
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = "#2e3440",
+}
+
+config.colors = {
+	tab_bar = {
+		-- The color of the inactive tab bar edge/divider
+		inactive_tab_edge = "#2e3440",
+
+		active_tab = {
+			bg_color = "#4C566A",
+			fg_color = "#FFFFFF",
+		},
+
+		inactive_tab = {
+			bg_color = "#2E3440",
+			fg_color = "#FFFFFF",
+		},
+
+		inactive_tab_hover = {
+			bg_color = "#3B4252",
+			fg_color = "#FFFFFF",
+		},
+
+		new_tab = {
+			bg_color = "#2e3440",
+			fg_color = "#FFFFFF",
+		},
+
+		new_tab_hover = {
+			bg_color = "#2e3440",
+			fg_color = "#FFFFFF",
+		},
+	},
+}
+
 return config
