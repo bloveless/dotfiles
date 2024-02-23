7927c75028e10137450f2303fbc36f81ae5f0931
Brennon Loveless <brennon.loveless.ext@bayer.com>
2024-02-23 14:06:20 -0800

    feat: added ideavim. updated wezterm font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,7 @@
 local wezterm = require("wezterm")
 
+local config = wezterm.config_builder()
+
 local mykeys = {
 	{ key = "{", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(-1) },
 	{ key = "}", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(1) },
@@ -14,18 +16,20 @@ for i = 1, 8 do
 	})
 end
 
-return {
-	-- color_scheme = "Catppuccin Macchiato", -- or Macchiato, Frappe, Latte
-	-- color_scheme = "tokyonight_storm",
-	color_scheme = "nord",
-	-- color_scheme = "Gruvbox dark, medium (base16)",
-	-- color_scheme = "Kanagawa (Gogh)",
-	-- color_scheme = "rose-pine-moon",
-	-- font = wezterm.font "MonoLisa Nerd Font",
-	-- font = wezterm.font "MesloLGS NF",
-	font_size = 12,
-	initial_cols = 150,
-	initial_rows = 45,
-	scrollback_lines = 50000,
-	keys = mykeys,
-}
+-- color_scheme = "Catppuccin Macchiato", -- or Macchiato, Frappe, Latte
+-- color_scheme = "tokyonight_storm",
+config.color_scheme = "nord"
+-- color_scheme = "Gruvbox dark, medium (base16)",
+-- color_scheme = "Kanagawa (Gogh)",
+-- color_scheme = "rose-pine-moon",
+-- font = wezterm.font "MonoLisa Nerd Font",
+-- font = wezterm.font "MesloLGS NF",
+config.font = wezterm.font("MonaspiceXe Nerd Font")
+config.font_size = 11
+config.initial_cols = 150
+config.initial_rows = 45
+config.scrollback_lines = 50000
+config.keys = mykeys
+config.tab_bar_at_bottom = true
+
+return config
