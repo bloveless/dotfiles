a0e09bd2c7681d1c8afee444445e2cc4685bb6e3
Brennon Loveless <brennon.loveless.ext@bayer.com>
2024-03-25 09:41:17 -0700

    feat: updated lualine to show full relative path instead of a truncated version

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -90,6 +90,35 @@ return {
     },
   },
 
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local icons = require("lazyvim.config").icons
+      return vim.tbl_deep_extend("force", opts, {
+        sections = {
+          lualine_c = {
+            LazyVim.lualine.root_dir(),
+            {
+              "diagnostics",
+              symbols = {
+                error = icons.diagnostics.Error,
+                warn = icons.diagnostics.Warn,
+                info = icons.diagnostics.Info,
+                hint = icons.diagnostics.Hint,
+              },
+            },
+            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+            {
+              "filename",
+              file_status = true,
+              path = 1,
+            },
+          },
+        },
+      })
+    end,
+  },
+
   -- Configure LazyVim to load nord
   {
     "LazyVim/LazyVim",
