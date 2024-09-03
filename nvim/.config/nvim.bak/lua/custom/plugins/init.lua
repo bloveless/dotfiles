8db50edf09b257cc4dcc210d964644422eb34987
Brennon Loveless <brennon.loveless.ext@bayer.com>
2024-09-03 14:31:19 -0700

    feat: very simple nvim kickstart

--- nvim/.config/nvim.bak/lua/custom/plugins/init.lua
+++ nvim/.config/nvim.bak/lua/custom/plugins/init.lua
@@ -1,5 +1,270 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+-- Session management
+vim.keymap.set('n', '<leader>qs', [[<cmd>lua require("persistence").load()<cr>]], { desc = 'Restore the session for the current directory' })
+vim.keymap.set('n', '<leader>ql', [[<cmd>lua require("persistence").load({ last = true })<cr>]], { desc = 'Restore the last session' })
+vim.keymap.set('n', '<leader>qd', [[<cmd>lua require("persistence").stop()<cr>]], { desc = "stop Persistence => session won't be saved on exit" })
+
+return {
+  {
+    'folke/persistence.nvim',
+    event = 'BufReadPre', -- this will only start session saving when an actual file was opened
+    opts = {},
+  },
+
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+
+  -- nvim v0.8.0
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+
+  -- Lazy
+  {
+    'crnvl96/lazydocker.nvim',
+    event = 'VeryLazy',
+    opts = {}, -- automatically calls `require("lazydocker").setup()`
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+    },
+    keys = {
+      { '<leader>gd', '<cmd>LazyDocker<cr>', desc = 'LazyDocker' },
+    },
+  },
+
+  {
+    'folke/trouble.nvim',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+    opts = {
+      modes = {
+        diagnostics = {
+          mode = 'diagnostics',
+          preview = {
+            type = 'split',
+            relative = 'win',
+            position = 'right',
+            size = 0.5,
+          },
+        },
+        quickfix = {
+          mode = 'quickfix',
+          preview = {
+            type = 'split',
+            relative = 'win',
+            position = 'right',
+            size = 0.5,
+          },
+        },
+        telescope = {
+          mode = 'telescope',
+          preview = {
+            type = 'split',
+            relative = 'win',
+            position = 'right',
+            size = 0.5,
+          },
+        },
+      },
+    },
+  },
+
+  {
+    'ruifm/gitlinker.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+      {
+        '<leader>gy',
+        function()
+          require('gitlinker').get_buf_range_url 'n'
+        end,
+        mode = { 'n', 'v' },
+        desc = '[g]it permalink [y]ank',
+      },
+    },
+    opts = {
+      mappings = nil,
+    },
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function(_, opts)
+      local config = require('lualine').get_config()
+      config = vim.tbl_deep_extend('force', config, {
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              file_status = true,
+              path = 1,
+            },
+          },
+        },
+      })
+
+      require('lualine').setup(config)
+    end,
+  },
+
+  -- necessary for blade indentation
+  'jwalton512/vim-blade',
+
+  {
+    'akinsho/bufferline.nvim',
+    event = 'VeryLazy',
+    keys = {
+      { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Toggle Pin' },
+      { '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Delete Non-Pinned Buffers' },
+      { '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', desc = 'Delete Other Buffers' },
+      { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete Buffers to the Right' },
+      { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
+      { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+      { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+      { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+      { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+      { '[B', '<cmd>BufferLineMovePrev<cr>', desc = 'Move buffer prev' },
+      { ']B', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer next' },
+    },
+    opts = {
+      options = {
+        close_command = function(n)
+          require('mini.bufremove').delete(n, false)
+        end,
+        right_mouse_command = function(n)
+          require('mini.bufremove').delete(n, false)
+        end,
+        diagnostics = 'nvim_lsp',
+        always_show_bufferline = true,
+      },
+    },
+    config = function(_, opts)
+      require('bufferline').setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd({ 'BufAdd', 'BufDelete' }, {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
+  },
+
+  -- {
+  --   'romgrk/barbar.nvim',
+  --   dependencies = {
+  --     'nvim-tree/nvim-web-devicons', -- patched fonts support
+  --     'lewis6991/gitsigns.nvim', -- display git status
+  --   },
+  --   init = function()
+  --     vim.g.barbar_auto_setup = false
+  --   end,
+  --   config = function()
+  --     local barbar = require 'barbar'
+  --
+  --     barbar.setup {
+  --       clickable = true, -- Enables/disables clickable tabs
+  --       tabpages = false, -- Enable/disables current/total tabpages indicator (top right corner)
+  --       insert_at_end = true,
+  --       icons = {
+  --         button = '',
+  --         buffer_index = true,
+  --         filetype = { enabled = true },
+  --         visible = { modified = { buffer_number = false } },
+  --         gitsigns = {
+  --           added = { enabled = true, icon = '+' },
+  --           changed = { enabled = true, icon = '~' },
+  --           deleted = { enabled = true, icon = '-' },
+  --         },
+  --       },
+  --     }
+  --
+  --     -- key maps
+  --
+  --     local map = vim.api.nvim_set_keymap
+  --     local opts = { noremap = true, silent = true }
+  --
+  --     -- Move to previous/next
+  --     map('n', 'H', '<Cmd>BufferPrevious<CR>', opts)
+  --     map('n', 'L', '<Cmd>BufferNext<CR>', opts)
+  --     -- Re-order to previous/next
+  --     -- map('n', '<A-H>', '<Cmd>BufferMovePrevious<CR>', opts)
+  --     -- map('n', '<A-L>', '<Cmd>BufferMoveNext<CR>', opts)
+  --     -- Goto buffer in position...
+  --     -- map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+  --     -- map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+  --     -- map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+  --     -- map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+  --     -- map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+  --     -- map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+  --     -- map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+  --     -- map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+  --     -- map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+  --     -- map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+  --     -- Pin/unpin buffer
+  --     -- map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+  --     -- Close buffer
+  --     map('n', '<C-c>', '<Cmd>BufferClose<CR>', opts)
+  --     -- map('n', '<A-b>', '<Cmd>BufferCloseAllButCurrent<CR>', opts)
+  --   end,
+  -- },
+}
--- nvim/.config/nvim.bak/lua/kickstart/plugins/debug.lua
+++ nvim/.config/nvim.bak/lua/kickstart/plugins/debug.lua
@@ -1,37 +1,74 @@
+-- debug.lua
+--
+-- Shows how to use the DAP plugin to debug your code.
+--
+-- Primarily focused on configuring the debugger for Go, but can
+-- be extended to other languages as well. That's why it's called
+-- kickstart.nvim and not kitchen-sink.nvim ;)
+
 return {
+  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
+  -- NOTE: And you can specify dependencies as well
   dependencies = {
+    -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
+
+    -- Required dependency for nvim-dap-ui
+    'nvim-neotest/nvim-nio',
+
+    -- Installs the debug adapters for you
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
-    'leoluz/nvim-dap-go',
-    'nvim-neotest/nvim-nio',
+
     'theHamsta/nvim-dap-virtual-text',
+
+    -- Add your own debuggers here
+    'leoluz/nvim-dap-go',
   },
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
 
     require('mason-nvim-dap').setup {
-      automatic_setup = true,
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_installation = true,
+
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
       handlers = {},
+
+      -- You'll need to check that you have the required things installed
+      -- online, please don't ask me how to install them :)
       ensure_installed = {
+        -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'php-debug-adapter',
       },
     }
 
     -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<leader>dc', dap.continue, { desc = '[d]ebug: Start/[c]ontinue' })
-    vim.keymap.set('n', '<leader>di', dap.step_into, { desc = '[d]ebug: Step [i]nto' })
-    vim.keymap.set('n', '<leader>do', dap.step_over, { desc = '[d]ebug: Step [o]ver' })
-    vim.keymap.set('n', '<leader>dO', dap.step_out, { desc = '[d]ebug: Step [O]ut' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = '[d]ebug: Toggle [b]reakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
+    vim.keymap.set('n', '<leader>dc', dap.continue, { desc = '[D]ebug: Start/[C]ontinue' })
+    vim.keymap.set('n', '<leader>di', dap.step_into, { desc = '[D]ebug: Step [I]nto' })
+    vim.keymap.set('n', '<leader>do', dap.step_over, { desc = '[D]ebug: Step [O]ver' })
+    vim.keymap.set('n', '<leader>dt', dap.step_out, { desc = '[D]ebug: Step Ou[t]' })
+    vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = '[D]ebug: Toggle [B]reakpoint' })
+    vim.keymap.set('n', '<leader>dB', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    end, { desc = '[d]ebug: Set Conditional [B]reakpoint' })
+    end, { desc = '[D]ebug: Set [B]reakpoint' })
+    vim.keymap.set('n', '<leader>dq', function()
+      dap.disconnect()
+      dap.close()
+      dapui.close()
+    end, { desc = '[D]ebug: [q]uit/close' })
 
     -- Dap UI setup
+    -- For more information, see |:help nvim-dap-ui|
     dapui.setup {
+      -- Set icons to characters that are more likely to work in every terminal.
+      --    Feel free to remove or use ones that you like more! :)
+      --    Don't feel like these are good choices.
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
       controls = {
         icons = {
@@ -55,9 +92,27 @@ return {
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-    -- Install golang specific config
-    require('dap-go').setup()
+    dap.configurations.php = {
+      {
+        type = 'php',
+        request = 'launch',
+        name = 'Listen for Xdebug',
+        port = 9003,
+        pathMappings = {
+          ['/var/www/html/'] = '/Users/brennon/Projects/sixpac/business-portal/',
+        },
+      },
+    }
 
-    require('nvim-dap-virtual-text').setup()
+    -- Install golang specific config
+    require('dap-go').setup {
+      delve = {
+        -- On Windows delve must be run attached or it crashes.
+        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+        detached = vim.fn.has 'win32' == 0,
+      },
+    }
+
+    require('nvim-dap-virtual-text').setup {}
   end,
 }
