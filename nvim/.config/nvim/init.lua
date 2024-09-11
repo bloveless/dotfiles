0a0aec8828a88ff80a8bf5a110ee60e4398e94fe
Brennon Loveless <brennon.loveless.ext@bayer.com>
2024-09-11 10:05:18 -0700

    feat: added neotest and golang configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -200,7 +200,7 @@ require("lazy").setup({
 				-- set icon mappings to true if you have a Nerd Font
 				mappings = vim.g.have_nerd_font,
 				-- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-				-- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
+				-- default which-key.nvim defined Nerd Font icons, otherwise define a string table
 				keys = vim.g.have_nerd_font and {} or {
 					Up = "<Up> ",
 					Down = "<Down> ",
@@ -252,6 +252,7 @@ require("lazy").setup({
 		branch = "0.1.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
+			"folke/trouble.nvim",
 			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
 				"nvim-telescope/telescope-fzf-native.nvim",
 
@@ -290,18 +291,20 @@ require("lazy").setup({
 			-- Telescope picker. This is really useful to discover what Telescope can
 			-- do as well as how to actually do it!
 
+			local open_with_trouble = require("trouble.sources.telescope").open
+
 			-- [[ Configure Telescope ]]
 			-- See `:help telescope` and `:help telescope.setup()`
 			require("telescope").setup({
 				-- You can put your default mappings / updates / etc. in here
 				--  All the info you're looking for is in `:help telescope.setup()`
 				--
-				-- defaults = {
-				--   mappings = {
-				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-				--   },
-				-- },
-				-- pickers = {}
+				defaults = {
+					mappings = {
+						i = { ["<c-t>"] = open_with_trouble },
+						n = { ["<c-t>"] = open_with_trouble },
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
@@ -521,7 +524,7 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
-				-- gopls = {}, -- included with go.nvim
+				gopls = {},
 				phpactor = {},
 				helm_ls = {},
 				docker_compose_language_service = {},
@@ -571,7 +574,7 @@ require("lazy").setup({
 				"blade-formatter",
 				-- "cspell",
 				"gofumpt",
-				"goimports-reviser",
+				"goimports",
 				"golangci-lint",
 				"hadolint",
 				"markdownlint",
@@ -613,6 +616,7 @@ require("lazy").setup({
 			},
 		},
 		opts = {
+			-- log_level = vim.log.levels.DEBUG,
 			notify_on_error = false,
 			format_after_save = function(bufnr)
 				-- Disable "format_on_save lsp_fallback" for languages that don't
@@ -630,21 +634,14 @@ require("lazy").setup({
 				}
 			end,
 			formatters = {
-				["goimports-reviser"] = {
-					prepend_args = {
-						"-company-prefixes",
-						"github.com/bayer-int",
-						"-imports-order",
-						"std,general,company",
-						"-rm-unused",
-						"-use-cache",
-					},
+				goimports = {
+					prepend_args = { "-local", "github.com/bayer-int" },
 				},
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
 				go = {
-					"goimports-reviser",
+					"goimports",
 					"gofumpt",
 				},
 				php = { "pint" },
@@ -987,6 +984,9 @@ require("lazy").setup({
 				"c",
 				"diff",
 				"go",
+				"gomod",
+				"gosum",
+				"gotmpl",
 				"hcl",
 				"html",
 				"lua",
@@ -1120,18 +1120,223 @@ require("lazy").setup({
 	},
 
 	{
-		"ray-x/go.nvim",
-		dependencies = { -- optional packages
-			"ray-x/guihua.lua",
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/nvim-treesitter",
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
 		},
-		config = function()
-			require("go").setup()
+	},
+
+	{
+		"nvim-neotest/neotest",
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>t", "", desc = "+test" },
+			{
+				"<leader>tt",
+				function()
+					require("neotest").run.run(vim.fn.expand("%"))
+				end,
+				desc = "Run File",
+			},
+			{
+				"<leader>tT",
+				function()
+					require("neotest").run.run(vim.uv.cwd())
+				end,
+				desc = "Run All Test Files",
+			},
+			{
+				"<leader>tr",
+				function()
+					require("neotest").run.run()
+				end,
+				desc = "Run Nearest",
+			},
+			{
+				"<leader>tl",
+				function()
+					require("neotest").run.run_last()
+				end,
+				desc = "Run Last",
+			},
+			{
+				"<leader>ts",
+				function()
+					require("neotest").summary.toggle()
+				end,
+				desc = "Toggle Summary",
+			},
+			{
+				"<leader>to",
+				function()
+					require("neotest").output.open({ enter = true, auto_close = true })
+				end,
+				desc = "Show Output",
+			},
+			{
+				"<leader>tO",
+				function()
+					require("neotest").output_panel.toggle()
+				end,
+				desc = "Toggle Output Panel",
+			},
+			{
+				"<leader>tS",
+				function()
+					require("neotest").run.stop()
+				end,
+				desc = "Stop",
+			},
+			{
+				"<leader>tw",
+				function()
+					require("neotest").watch.toggle(vim.fn.expand("%"))
+				end,
+				desc = "Toggle Watch",
+			},
+		},
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"fredrikaverpil/neotest-golang", -- Installation
+		},
+		opts = {
+			adapters = {
+				["neotest-golang"] = {
+					go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
+					dap_go_enabled = true,
+				},
+			},
+			status = {
+				virtual_text = false,
+			},
+			output = {
+				open_on_run = true,
+			},
+			quickfix = {
+				open = function()
+					require("trouble").open({ mode = "quickfix", focus = false })
+				end,
+			},
+		},
+		config = function(_, opts)
+			local neotest_ns = vim.api.nvim_create_namespace("neotest")
+			vim.diagnostic.config({
+				virtual_text = {
+					format = function(diagnostic)
+						print("diagnostics" .. vim.inspect(diagnostic))
+						-- Replace newline and tab characters with space for more compact diagnostics
+						local message =
+							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+						return message
+					end,
+				},
+			}, neotest_ns)
+
+			opts.consumers = opts.consumers or {}
+
+			-- Refresh and auto close trouble after running tests
+			---@type neotest.Consumer
+			opts.consumers.trouble = function(client)
+				client.listeners.results = function(adapter_id, results, partial)
+					if partial then
+						return
+					end
+					local tree = assert(client:get_position(nil, { adapter = adapter_id }))
+
+					local failed = 0
+					for pos_id, result in pairs(results) do
+						if result.status == "failed" and tree:get_key(pos_id) then
+							failed = failed + 1
+						end
+					end
+					vim.schedule(function()
+						local trouble = require("trouble")
+						if trouble.is_open() then
+							trouble.refresh()
+							if failed == 0 then
+								trouble.close()
+							end
+						end
+					end)
+					return {}
+				end
+			end
+
+			if opts.adapters then
+				local adapters = {}
+				for name, config in pairs(opts.adapters or {}) do
+					if type(name) == "number" then
+						if type(config) == "string" then
+							config = require(config)
+						end
+						adapters[#adapters + 1] = config
+					elseif config ~= false then
+						local adapter = require(name)
+						if type(config) == "table" and not vim.tbl_isempty(config) then
+							local meta = getmetatable(adapter)
+							if adapter.setup then
+								adapter.setup(config)
+							elseif adapter.adapter then
+								adapter.adapter(config)
+								adapter = adapter.adapter
+							elseif meta and meta.__call then
+								adapter(config)
+							else
+								error("Adapter " .. name .. " does not support setup")
+							end
+						end
+						adapters[#adapters + 1] = adapter
+					end
+				end
+				opts.adapters = adapters
+			end
+
+			require("neotest").setup(opts)
 		end,
-		event = { "CmdlineEnter" },
-		ft = { "go", "gomod" },
-		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
+
+	{
+		"maxandron/goplements.nvim",
+		ft = "go",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
 	},
 }, {
 	ui = {
