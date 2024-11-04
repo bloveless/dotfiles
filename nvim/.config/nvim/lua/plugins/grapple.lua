f62e812fc28aaf6108c53c2c47da8867a3d3a256
Brennon Loveless <brennon.loveless@gmail.com>
2024-11-03 20:34:35 -0800

    feat: update grapple to show more marks. removed unused themes

--- nvim/.config/nvim/lua/plugins/grapple.lua
+++ nvim/.config/nvim/lua/plugins/grapple.lua
@@ -14,6 +14,10 @@ return {
 			{ "<leader>3", "<cmd>Grapple select index=3<cr>", { desc = "Grapple cycle index three" } },
 			{ "<leader>4", "<cmd>Grapple select index=4<cr>", { desc = "Grapple cycle index four" } },
 			{ "<leader>5", "<cmd>Grapple select index=5<cr>", { desc = "Grapple cycle index five" } },
+			{ "<leader>6", "<cmd>Grapple select index=6<cr>", { desc = "Grapple cycle index six" } },
+			{ "<leader>7", "<cmd>Grapple select index=7<cr>", { desc = "Grapple cycle index seven" } },
+			{ "<leader>8", "<cmd>Grapple select index=8<cr>", { desc = "Grapple cycle index eight" } },
+			{ "<leader>9", "<cmd>Grapple select index=9<cr>", { desc = "Grapple cycle index nine" } },
 		},
 		opts = {
 			scope = "git_branch",
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -23,23 +23,9 @@ return {
 				grey = "#303030",
 			}
 
-			local bubbles_theme = {
-				normal = {
-					a = { fg = colors.black, bg = colors.violet },
-					b = { fg = colors.white, bg = colors.grey },
-					c = { fg = colors.white },
-				},
-
-				insert = { a = { fg = colors.black, bg = colors.blue } },
-				visual = { a = { fg = colors.black, bg = colors.cyan } },
-				replace = { a = { fg = colors.black, bg = colors.red } },
-
-				inactive = {
-					a = { fg = colors.white, bg = colors.black },
-					b = { fg = colors.white, bg = colors.black },
-					c = { fg = colors.white },
-				},
-			}
+			require("grapple-line").setup({
+				number_of_files = 10,
+			})
 
 			require("lualine").setup({
 				options = {
