a290f6e0572531d8bead8f995d60cc6aa07111a9
Brennon Loveless <brennon.loveless.ext@bayer.com>
2024-11-11 11:31:18 -0800

    feat: back to lazyvim

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,19 +1,13 @@
 return {
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		enabled = true,
-		checkout = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended (uses mini.icons)
-			"MunifTanjim/nui.nvim",
-			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		},
-		opts = {
-			close_if_last_window = false,
-		},
-		keys = {
-			{ "<leader>e", "<cmd>Neotree filesystem reveal toggle<cr>", { desc = "File explorer" } },
-		},
-	},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      close_if_last_window = false,
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+  },
 }
--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,142 +1,21 @@
 return {
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"nvim-lua/plenary.nvim",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"fredrikaverpil/neotest-golang",
-			"V13Axel/neotest-pest",
-		},
-		keys = {
-			{
-				"<leader>tt",
-				function()
-					require("neotest").output_panel.clear()
-					require("neotest").run.run(vim.fn.expand("%"))
-				end,
-				{ desc = "Run File" },
-			},
-			{
-				"<leader>tT",
-				function()
-					require("neotest").output_panel.clear()
-					require("neotest").run.run(vim.uv.cwd())
-				end,
-				{ desc = "Run All Test Files" },
-			},
-			{
-				"<leader>tr",
-				function()
-					require("neotest").output_panel.clear()
-					require("neotest").run.run()
-				end,
-				{ desc = "Run Nearest" },
-			},
-			{
-				"<leader>tdr",
-				function()
-					require("neotest").run.run({ strategy = "dap" })
-				end,
-				{ desc = "Debug nearest" },
-			},
-			{
-				"<leader>tdl",
-				function()
-					require("neotest").run.run_last({ strategy = "dap" })
-				end,
-				{ desc = "Debug last" },
-			},
-			{
-				"<leader>tl",
-				function()
-					require("neotest").output_panel.clear()
-					require("neotest").run.run_last()
-				end,
-				{ desc = "Run Last" },
-			},
-			{
-				"<leader>ts",
-				function()
-					require("neotest").summary.toggle()
-				end,
-				{ desc = "Toggle Summary" },
-			},
-			{
-				"<leader>to",
-				function()
-					require("neotest").output.open({ enter = false, auto_close = true })
-				end,
-				{ desc = "Show Output" },
-			},
-			{
-				"<leader>tO",
-				function()
-					require("neotest").output_panel.toggle()
-				end,
-				{ desc = "Toggle Output Panel" },
-			},
-			{
-				"<leader>tS",
-				function()
-					require("neotest").run.stop()
-				end,
-				{ desc = "Stop" },
-			},
-			{
-				"<leader>tw",
-				function()
-					require("neotest").watch.toggle(vim.fn.expand("%"))
-				end,
-				{ desc = "Toggle Watch" },
-			},
-		},
-		opts = {
-			-- this will log to `:lua vim.print(require("neotest.logging"):get_filename())`
-			-- log_level = vim.log.levels.DEBUG,
-			-- See all config options with :h neotest.Config (options from https://github.com/fredrikaverpil/neotest-golang to help neotest performance in large code bases)
-			discovery = {
-				-- Drastically improve performance in ginormous projects by
-				-- only AST-parsing the currently opened buffer.
-				enabled = false,
-				-- Number of workers to parse files concurrently.
-				-- A value of 0 automatically assigns number based on CPU.
-				-- Set to 1 if experiencing lag.
-				concurrent = 0,
-			},
-			running = {
-				-- Run tests concurrently when an adapter provides multiple commands to run.
-				concurrent = true,
-			},
-			summary = {
-				-- Enable/disable animation of icons.
-				animated = true,
-			},
-			status = {
-				virtual_text = false,
-			},
-			output = {
-				open_on_run = false,
-			},
-			output_summary = {
-				enabled = true,
-			},
-		},
-		config = function(_, opts)
-			opts.adapters = {
-				require("neotest-golang")({
-					-- go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
-					-- go_test_args = { "-cover", "-short" },
-					go_test_args = { "-cover" },
-					dap_go_enabled = true,
-				}),
-				require("neotest-pest")({
-					sail_enabled = false,
-				}),
-			}
-
-			require("neotest").setup(opts)
-		end,
-	},
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      -- See all config options with :h neotest.Config
+      discovery = {
+        -- Drastically improve performance in ginormous projects by
+        -- only AST-parsing the currently opened buffer.
+        enabled = false,
+        -- Number of workers to parse files concurrently.
+        -- A value of 0 automatically assigns number based on CPU.
+        -- Set to 1 if experiencing lag.
+        concurrent = 1,
+      },
+      running = {
+        -- Run tests concurrently when an adapter provides multiple commands to run.
+        concurrent = true,
+      },
+    },
+  },
 }
