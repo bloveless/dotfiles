f687ef4a77c88cf958688073e6556062b0b4f2c9
Brennon Loveless <brennon.loveless.ext@bayer.com>
2024-11-11 21:31:02 -0800

    feat: blink.cmp again

---
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = false,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "nvim_lsp_signature_help" })
+    end,
+  },
+}
