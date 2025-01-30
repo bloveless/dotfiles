f7c7aa213826b7ad562835c65b41e373745646a4
Brennon Loveless <brennon.loveless@gmail.com>
2025-01-29 20:38:34 -0800

    fix: update tabby to use a builtin preset

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -813,42 +813,9 @@ require("lazy").setup({
 		-- event = 'VimEnter', -- if you want lazy load, see below
 		dependencies = "nvim-tree/nvim-web-devicons",
 		config = function()
-			local theme = {
-				fill = "TabLineFill",
-				head = { fg = "#8aadf4", bg = "#24273a", style = "italic" },
-				current_tab = { fg = "#1e2030", bg = "#8aadf4", style = "italic" },
-				tab = { fg = "#8aadf4", bg = "#24273a", style = "italic" },
-				win = { fg = "#1e2030", bg = "#8aadf4", style = "italic" },
-				tail = { fg = "#8aadf4", bg = "#24273a", style = "italic" },
-			}
-
-			require("tabby.tabline").set(function(line)
-				return {
-					{
-						{ "  ", hl = theme.head },
-						line.sep("", theme.head, theme.fill), -- 
-					},
-					line.tabs().foreach(function(tab)
-						local hl = tab.is_current() and theme.current_tab or theme.tab
-						return {
-							line.sep("", hl, theme.fill), -- 
-							tab.is_current() and "" or "",
-							tab.number(),
-							tab.name(),
-							-- tab.close_btn(''), -- show a close button
-							line.sep("", hl, theme.fill), -- 
-							hl = hl,
-							margin = " ",
-						}
-					end),
-					line.spacer(),
-					{
-						line.sep("", theme.tail, theme.fill), -- 
-						{ "  ", hl = theme.tail },
-					},
-					hl = theme.fill,
-				}
-			end)
+			require("tabby").setup({
+				preset = "active_wins_at_tail",
+			})
 		end,
 	},
 
