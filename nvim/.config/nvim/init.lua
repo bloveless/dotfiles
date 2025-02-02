a45bdee9584d880153c819ff183770d88db80bab
Brennon Loveless <brennon.loveless@gmail.com>
2025-02-02 12:42:11 -0800

    feat: added dropbar

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -863,6 +863,16 @@ require("lazy").setup({
 	},
 
 	{
+		"Bekaboo/dropbar.nvim",
+		config = function()
+			local dropbar_api = require("dropbar.api")
+			vim.keymap.set("n", "<leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+			vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+			vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+		end,
+	},
+
+	{
 		"olimorris/codecompanion.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
