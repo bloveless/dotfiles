093a81b092945fdafc310e5f76a158b24121828f
Brennon Loveless <brennon.loveless@gmail.com>
2025-02-06 21:19:55 -0800

    feat(blink): automatically show documentation during completion

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -618,6 +618,13 @@ require("lazy").setup({
 			signature = {
 				enabled = true,
 			},
+
+			completion = {
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 500,
+				},
+			},
 		},
 		opts_extend = { "sources.default" },
 	},
