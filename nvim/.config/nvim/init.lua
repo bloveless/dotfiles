bb02646104d06fbb0c71df279c91eb60b7eb554e
Brennon Loveless <brennon.loveless.ext@bayer.com>
2025-02-07 10:36:23 -0800

    feat: added github.com/bloveless to go locals list

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -439,7 +439,7 @@ require("lazy").setup({
 				gopls = {
 					settings = {
 						gopls = {
-							["local"] = "github.com/bayer-int,github.com/shadowglass-xyz",
+							["local"] = "github.com/bayer-int,github.com/shadowglass-xyz,github.com/bloveless",
 							gofumpt = true,
 							codelenses = {
 								gc_details = false,
@@ -970,6 +970,50 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
 			vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
 			vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+		end,
+	},
+
+	{
+		"milanglacier/minuet-ai.nvim",
+		config = function()
+			require("minuet").setup({
+				virtualtext = {
+					auto_trigger_ft = {},
+					keymap = {
+						-- accept whole completion
+						accept = "<A-A>",
+						-- accept one line
+						accept_line = "<A-a>",
+						-- accept n lines (prompts for number)
+						accept_n_lines = "<A-z>",
+						-- Cycle to prev completion item, or manually invoke completion
+						prev = "<A-[>",
+						-- Cycle to next completion item, or manually invoke completion
+						next = "<A-]>",
+						dismiss = "<A-e>",
+					},
+				},
+				provider = "openai_fim_compatible",
+				n_completions = 1, -- recommend for local model for resource saving
+				-- I recommend beginning with a small context window size and incrementally
+				-- expanding it, depending on your local computing power. A context window
+				-- of 512, serves as an good starting point to estimate your computing
+				-- power. Once you have a reliable estimate of your local computing power,
+				-- you should adjust the context window to a larger value.
+				context_window = 512,
+				provider_options = {
+					openai_fim_compatible = {
+						api_key = "TERM",
+						name = "Ollama",
+						end_point = "http://localhost:11434/v1/completions",
+						model = "qwen2.5-coder:14b",
+						optional = {
+							max_tokens = 256,
+							top_p = 0.9,
+						},
+					},
+				},
+			})
 		end,
 	},
 
