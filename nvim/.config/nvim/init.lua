4b7c3439f087ee8e7b0f296dc9a9d14f0943767c
Brennon Loveless <brennon.loveless.ext@bayer.com>
2025-06-18 09:44:35 -0700

    gruvbox today

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -401,16 +401,18 @@ require("lazy").setup({
 
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 },
+
 	{
 		"f-person/auto-dark-mode.nvim",
 		opts = {
 			set_dark_mode = function()
 				vim.api.nvim_set_option_value("background", "dark", {})
-				vim.cmd("colorscheme catppuccin-macchiato")
+				vim.cmd("colorscheme gruvbox")
 			end,
 			set_light_mode = function()
 				vim.api.nvim_set_option_value("background", "light", {})
-				vim.cmd("colorscheme catppuccin-latte")
+				vim.cmd("colorscheme gruvbox")
 			end,
 			update_interval = 3000,
 			fallback = "dark",
