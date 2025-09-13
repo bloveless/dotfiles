485da5651dc28e9303b7da0726fa2d4de22754c1
Brennon Loveless <brennon.loveless@gmail.com>
2025-09-13 09:36:40 -0700

    feat: changed wezterm to use a tab bar plugin. changed wezterm and zed to use jetbrains mono

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,4 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
 
 local function active_tab_idx(mux_win)
@@ -68,84 +67,164 @@ for i = 1, 8 do
 	})
 end
 
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Macchiato"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
 -- config.color_scheme = "Gruvbox dark, soft (base16)"
 -- config.color_scheme = "OneDark (base16)"
-config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+-- local theme = wezterm.color.get_builtin_schemes()[scheme_for_appearance(wezterm.gui.get_appearance())]
 
-config.font = wezterm.font("MonoLisa")
--- config.font = wezterm.font("JetBrainsMono Nerd Font")
+-- config.font = wezterm.font("MonoLisa")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 -- config.font = wezterm.font("MesloLGS NF")
 -- config.font = wezterm.font("FiraCode Nerd Font")
 -- config.font = wezterm.font("Monaspace Argon Var")
 -- config.font = wezterm.font("Hack", { weight = "Regular" })
-config.font_size = 11
+config.font_size = 13
 config.line_height = 1.4
 config.initial_cols = 150
 config.initial_rows = 45
 config.scrollback_lines = 100000
 config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.tab_max_width = 32
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE|MACOS_USE_BACKGROUND_COLOR_AS_TITLEBAR_COLOR"
+-- config.window_decorations = "TITLE|RESIZE"
 
--- local theme = require("lua/rose-pine").moon
-local theme = wezterm.color.get_builtin_schemes()["Catppuccin Macchiato"]
--- local theme = wezterm.color.get_builtin_schemes()["OneDark (base16)"]
+-- config.window_frame = {
+-- The font used in the tab bar.
+-- Roboto Bold is the default; this font is bundled
+-- with wezterm.
+-- Whatever font is selected here, it will have the
+-- main font setting appended to it to pick up any
+-- fallback fonts you may have used there.
+-- font = wezterm.font({ family = "MonoLisa", weight = "Bold" }),
 
-config.window_frame = {
-	-- The font used in the tab bar.
-	-- Roboto Bold is the default; this font is bundled
-	-- with wezterm.
-	-- Whatever font is selected here, it will have the
-	-- main font setting appended to it to pick up any
-	-- fallback fonts you may have used there.
-	-- font = wezterm.font({ family = "MonoLisa", weight = "Bold" }),
+-- The size of the font in the tab bar.
+-- Default to 10.0 on Windows but 12.0 on other systems
+-- font_size = 13.0,
 
-	-- The size of the font in the tab bar.
-	-- Default to 10.0 on Windows but 12.0 on other systems
-	font_size = 11.0,
+-- The overall background color of the tab bar when
+-- the window is focused
+-- active_titlebar_bg = theme.background,
 
-	-- The overall background color of the tab bar when
-	-- the window is focused
-	active_titlebar_bg = theme.background,
+-- The overall background color of the tab bar when
+-- the window is not focused
+-- inactive_titlebar_bg = theme.background,
+-- }
 
-	-- The overall background color of the tab bar when
-	-- the window is not focused
-	inactive_titlebar_bg = theme.background,
-}
+-- The filled in variant of the < symbol
+-- local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
 
-config.colors = {
-	tab_bar = {
-		active_tab = {
-			bg_color = theme.ansi[3],
-			fg_color = "#333",
-		},
+-- The filled in variant of the > symbol
+-- local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
 
-		inactive_tab_edge = theme.ansi[1],
+-- config.tab_bar_style = {
+-- 	active_tab_left = wezterm.format({
+-- 		{ Background = { Color = "#0b0022" } },
+-- 		{ Foreground = { Color = "#2b2042" } },
+-- 		{ Text = SOLID_LEFT_ARROW },
+-- 	}),
+-- 	active_tab_right = wezterm.format({
+-- 		{ Background = { Color = "#0b0022" } },
+-- 		{ Foreground = { Color = "#2b2042" } },
+-- 		{ Text = SOLID_RIGHT_ARROW },
+-- 	}),
+--
+-- 	inactive_tab_left = wezterm.format({
+-- 		{ Background = { Color = "#0b0022" } },
+-- 		{ Foreground = { Color = "#1b1032" } },
+-- 		{ Text = SOLID_LEFT_ARROW },
+-- 	}),
+-- 	inactive_tab_right = wezterm.format({
+-- 		{ Background = { Color = "#0b0022" } },
+-- 		{ Foreground = { Color = "#1b1032" } },
+-- 		{ Text = SOLID_RIGHT_ARROW },
+-- 	}),
+-- }
 
-		inactive_tab = {
-			bg_color = theme.background,
-			fg_color = "#EEE",
-		},
-
-		new_tab = {
-			fg_color = "#EEE",
-			bg_color = theme.background,
-		},
-	},
-}
+-- config.colors = {
+-- 	tab_bar = {
+-- 		active_tab = {
+-- 			bg_color = theme.ansi[3],
+-- 			fg_color = "#333",
+-- 		},
+--
+-- 		inactive_tab_edge = theme.ansi[1],
+--
+-- 		inactive_tab = {
+-- 			bg_color = theme.background,
+-- 			fg_color = "#EEE",
+-- 		},
+--
+-- 		new_tab = {
+-- 			fg_color = "#EEE",
+-- 			bg_color = theme.background,
+-- 		},
+-- 	},
+-- }
 
 -- config.colors = theme.colors()
 -- config.window_frame = theme.window_frame() -- needed only if using fancy tab bar
 
-config.window_padding = { left = 10, right = 10, top = 0, bottom = 0 }
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+--
+-- wezterm.on("update-right-status", function(window)
+-- 	local time = wezterm.strftime("%H:%M")
+--
+-- 	window:set_right_status(wezterm.format({
+-- 		{ Background = { Color = theme.background } },
+-- 		{ Text = " " },
+-- 		{ Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+-- 		{ Text = "  " },
+-- 	}))
+-- end)
 
-wezterm.on("update-right-status", function(window)
-	local time = wezterm.strftime("%H:%M")
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+tabline.setup({
+	options = {
+		icons_enabled = true,
+		-- theme = scheme_for_appearance(wezterm.gui.get_appearance()),
+		tabs_enabled = true,
+		theme_overrides = {},
+		section_separators = {
+			left = wezterm.nerdfonts.ple_right_half_circle_thick,
+			right = wezterm.nerdfonts.ple_left_half_circle_thick,
+		},
+		component_separators = {
+			left = wezterm.nerdfonts.ple_right_half_circle_thin,
+			right = wezterm.nerdfonts.ple_left_half_circle_thin,
+		},
+		tab_separators = {
+			left = wezterm.nerdfonts.ple_right_half_circle_thick,
+			right = wezterm.nerdfonts.ple_left_half_circle_thick,
+		},
+	},
+	sections = {
+		tabline_a = {},
+		tabline_b = {},
+		tabline_c = {},
+		tab_active = {
+			"index",
+			{ "parent", padding = 0 },
+			"/",
+			{ "cwd", padding = { left = 0, right = 1 } },
+			{ "zoomed", padding = 0 },
+		},
+		tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
+		tabline_x = {},
+		tabline_y = { "cpu", "ram" },
+		tabline_z = { "datetime", "battery" },
+	},
+	extensions = {},
+})
 
-	window:set_right_status(wezterm.format({
-		{ Background = { Color = theme.background } },
-		{ Text = " " },
-		{ Text = wezterm.nerdfonts.md_clock .. "  " .. time },
-		{ Text = "  " },
-	}))
-end)
-
+-- tabline.apply_to_config(config)
 return config
