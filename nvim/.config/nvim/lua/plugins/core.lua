7fe128124e3e862c78eae764ca136894863c8f23
Brennon Loveless <brennon.loveless.ext@bayer.com>
2025-12-04 14:57:59 -0800

    feat: nvim back to lazyvim. updated a few ghostty settings

---
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              hints = {
+                assignVariableTypes = false,
+                compositeLiteralFields = false,
+                compositeLiteralTypes = false,
+                constantValues = false,
+                functionTypeParameters = false,
+                parameterNames = false,
+                rangeVariableTypes = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
